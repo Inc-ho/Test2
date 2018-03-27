70dd52c3473234a3b5a405b9b97848c92dc91323
githubqiuqiu <863729971@qq.com>
2018-03-27 19:31:46 +0800

    qiu：修改2

--- src/main/java/com/imooc/test/GitTest.java
+++ src/main/java/com/imooc/test/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("123!");
         System.out.println("lala");
+        System.out.println("xaxa");
     }
 
 
