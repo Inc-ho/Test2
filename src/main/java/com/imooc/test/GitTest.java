93b11b526ea453b61008b838d9bc534dd2f2aec6
Inc-ho <878014750@qq.com>
2018-03-27 19:12:19 +0800

    Initial commit

---
+++ src/main/java/com/imooc/test/GitTest.java
@@ -0,0 +1,14 @@
+package com.imooc.test;
+
+/**
+ * Create By Min
+ * 2018/3/27
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("123!");
+    }
+
+
+}
